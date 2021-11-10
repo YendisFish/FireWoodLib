4b3518f0d354ada1f794e25f4025a4638838696b
YendisFish <brintonsid@gmail.com>
2021-11-09 17:09:14 -0800

    Adding Changes

--- FireWoodLib/Types/LogFile.cs
+++ FireWoodLib/Types/LogFile.cs
@@ -1,7 +1,39 @@
-﻿namespace FireWoodLib.Types
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FireWoodLib.Types.UserHandler;
+
+namespace FireWoodLib.Types.Log
 {
     public class LogFile
     {
+        public string FileName { get; set; }
+        public string Data { get; set; }
+        public List<string> DataList { get; set; }
+
+        public LogFile(string fileName, string data, List<string> dataArray)
+        {
+            fileName = FileName;
+            data = Data;
+            dataArray = DataList;
+        }
+
+        public void Write(LogFile log, User user)
+        {
+            DateTime dateMade = DateTime.Now;
+
+            string ContentToWrite = dateMade.ToString() + " | " + user.Username + " | " + log.Data;
+            
+            File.AppendAllText(log.FileName, ContentToWrite);
+        }
         
+        public void WriteArray(LogFile log, User user)
+        {
+            DateTime dateMade = DateTime.Now;
+
+            string ContentToWrite = dateMade.ToString() + " | " + user.Username + " | " + log.DataList.ToString();
+
+            File.AppendAllText(log.FileName, ContentToWrite);
+        }
     }
 }
--- FireWoodLib/Types/User.cs
+++ FireWoodLib/Types/User.cs
@@ -1,7 +1,22 @@
-﻿namespace FireWoodLib.Types.Log
+﻿using System;
+
+namespace FireWoodLib.Types.UserHandler
 {
     public class User
     {
-        
+        public string Username { get; set; }
+        public string UserToken { get; set; }
+
+        public User(string username, string userToken)
+        {
+            username = Username;
+            userToken = UserToken;
+        }
+
+        public string[] CombineAllData()
+        {
+            string[] Combined = {Username, UserToken};
+            return Combined;
+        }
     }
 }
